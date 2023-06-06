fbcc5be0c806df13611471d09027fcd33fa5c72c
leonid <leonidsleo@yandex.ru>
2023-06-06 20:47:33 +0300

    решена задача 64, семинар 9, курс введ в прогр

--- C#_Seminar9_Homework_Zad64/Program.cs
+++ C#_Seminar9_Homework_Zad64/Program.cs
@@ -2,3 +2,14 @@
 // Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+void AllNaturalNumbers(int max, int min)
+{
+    if (min == max) Console.Write($"{max}, ");
+    else
+    {
+        AllNaturalNumbers(max, min + 1);
+        Console.Write($"{min}, ");
+    }
+}
+AllNaturalNumbers(10, 2);
